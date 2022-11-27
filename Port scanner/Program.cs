b383c6d15edc865bc18751b5d0f31c0adafc532e
Nicolai-Aardalen <Nicolaiaar@viken.no>
2022-11-27 15:23:34 +0100

    Add project files.

---
+++ Port scanner/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Port_scanner
+{
+    internal class Program
+    {
+        private static string IP = "";
+
+        static void Main(string[] args)
+        {
+            UserInput();
+            PortScan();
+            Console.ReadKey();
+        }
+
+        private static void UserInput()
+        {
+            Console.Write("IP Address: ");
+            IP = Console.ReadLine();
+        }
+
+        private static void PortScan()
+        {
+            Console.Clear();
+            TcpClient Scan = new TcpClient();
+            foreach (int s in Ports)
+            {
+                try
+                {
+                    Scan.Connect(IP, s);
+                    Console.WriteLine($"[{s}] | OPEN");
+                }
+                catch
+                {
+                    Console.WriteLine($"[{s}] | CLOSED");
+                }
+            }
+        }
+
+        private static int[] Ports = new int[]
+        {
+        21,
+        22,
+        80
+        };
+    }
+}
